c86292a4a8179ad5b7145a045d01608e8ef500fb
fxanhkhoa <fxanhkhoa@gmail.com>
2019-04-04 08:59:42 +0700

    add python example

---
+++ Round1/Window/example.py
@@ -0,0 +1,91 @@
+import cv2
+import socket
+import sys
+
+import json
+import threading
+import queue
+
+import time
+
+rspeed = 0
+speed = 0
+angle = 0
+path = 'window_version_Data/Snapshots/fx_UIT_Car.png'
+
+port = 9999
+ip = str(sys.argv[1])
+
+## Function Parse to Json String
+def jsonToString(speed, angle):
+   jsonObject = {
+      'speed': speed,
+      'angle': angle,
+      'request': 'SPEED',
+   }
+   jsonString = json.dumps(jsonObject)
+   # print(jsonString)
+   return jsonString
+
+## Thread for Socket communication
+class socketThread (threading.Thread):
+   def __init__(self, threadID, sock):
+      threading.Thread.__init__(self)
+      self.threadID = threadID
+      self.sock = sock
+   def run(self):
+      global speed
+      global angle
+      global rspeed
+      while(True):
+         try:
+            message = jsonToString(speed, angle)
+            sock.sendall(message.encode())
+            data = sock.recv(100).decode('ascii')
+            rspeed = int(data)
+            # print(speed)
+         except Exception as e:
+            print(e)
+            sys.exit(1)
+
+         # time.sleep(2)
+
+## Thread for Processing Image
+class processThread (threading.Thread):
+   def __init__(self, threadID):
+      threading.Thread.__init__(self)
+      self.threadID = threadID
+   def run(self):
+      global speed
+      global angle
+      while True:
+         try:
+            img = cv2.imread(path)
+            speed = 10
+            print('speed now is : {0}', rspeed)
+            if img is not None:
+               cv2.imshow('image', img)
+            
+               cv2.waitKey(1)
+         except Exception as e:
+            print(e)
+      cv2.destroyAllWindows()
+
+try:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((ip , port))
+    print("Connected to ", ip, ":", port)
+
+    ## image processing here
+except Exception as ex:
+    print(ex)
+    sys.exit()
+
+threadSendRecv = socketThread(1, sock)
+threadProcess = processThread(2)
+
+threadSendRecv.start()
+threadProcess.start()
+
+threadProcess.join()
+threadSendRecv.join()
